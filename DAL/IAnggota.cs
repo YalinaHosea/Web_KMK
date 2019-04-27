eb75244ac529ccf124eb1189c99f8bfb1f022ccc
Ivanbernov1999 <ivanbernov76@gmail.com>
2019-04-27 13:12:14 +0700

    dari ivan mau konek database

---
+++ DAL/IAnggota.cs
@@ -0,0 +1,16 @@
+
+
+using System.Collections.Generic;
+using Web_KMK.Models;
+
+namespace Web_KMK.DAL{
+    public interface IAnggota{
+        IEnumerable<Anggota> GetAll();
+
+        Anggota GetByid_anggota(string id_anggota);
+
+        void Insert(Anggota agt);
+
+        void Update(Anggota)
+    }
+}
